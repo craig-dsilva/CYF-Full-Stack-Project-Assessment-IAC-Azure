5ff4b44c6349cc6791ff4167ba8061deeab4ffe9
craig-dsilva <craig.s.dsilva@gmail.com>
2024-02-12 16:08:42 +0000

    SWA

--- swa.tf
+++ swa.tf
@@ -1,4 +1,4 @@
-resource "azurerm_static_site" "video-recommendations" {
+resource "azurerm_static_site" "video_recommendations" {
   name                = "cyf-videos"
   resource_group_name = azurerm_resource_group.cyf-videos.name
   location            = "West Europe"
