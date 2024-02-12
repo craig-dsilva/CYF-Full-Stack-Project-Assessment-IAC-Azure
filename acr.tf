98b3a4dfa6ce655aaac36ccaf93fa309285d13b7
craig-dsilva <craig.s.dsilva@gmail.com>
2024-02-12 22:05:52 +0000

    ACR

---
+++ acr.tf
@@ -0,0 +1,6 @@
+resource "azurerm_container_registry" "cyf_videos" {
+  name                = "cyfvideos"
+  resource_group_name = azurerm_resource_group.cyf-videos.name
+  location            = azurerm_resource_group.cyf-videos.location
+  sku                 = "Basic"
+}
