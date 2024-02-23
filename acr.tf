f318f6dfea559441d46df1df6f9ea301a556280d
craig-dsilva <craig.s.dsilva@gmail.com>
2024-02-23 14:28:41 +0000

    Switch ACR SKU to Standard

--- acr.tf
+++ acr.tf
@@ -2,7 +2,7 @@ resource "azurerm_container_registry" "cyf_videos" {
   name                = "cyfvideos"
   resource_group_name = azurerm_resource_group.cyf-videos.name
   location            = azurerm_resource_group.cyf-videos.location
-  sku                 = "Basic"
+  sku                 = "Standard"
   admin_enabled       = true
 
   tags = {
